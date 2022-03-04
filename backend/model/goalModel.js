fe4fd6c90a7d2de08a217e5cfb14083a4a27b415
KairatSS <kairat.seksembaev@gmail.com>
2022-03-05 03:58:40 +0800

    Initial rest api for goals

---
+++ backend/model/goalModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const goalSchema = mongoose.Schema({
+    text: {
+        type: 'string',
+        required: [true,'Please add a text field']
+    }
+}, {
+    timestamps: true,
+})
+module.exports =mongoose.model('Goal',goalSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config(); // access .env files with variables
 const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT||5000;
+connectDB();
 const app = express();
 
 //this serves as middleware so essentially called everytime req is made 
