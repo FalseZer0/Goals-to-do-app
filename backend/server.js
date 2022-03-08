b544e23a38dda1b8f889fe0ef18dda3733466f8e
KairatSS <kairat.seksembaev@gmail.com>
2022-03-09 02:29:54 +0800

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv').config(); // access .env files with variables
 const {errorHandler} = require('./middleware/errorMiddleware');
+const path = require('path');
 const port = process.env.PORT||5000;
 connectDB();
 const app = express();
@@ -14,6 +15,19 @@ app.use(express.urlencoded());
 app.use('/api/goals',require('./routes/goalRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+  }
+  
 app.use(errorHandler);
 
 app.listen(port,()=>{console.log(`server started on port ${port}`)});
